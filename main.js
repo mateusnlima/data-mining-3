9b916cd382e37d37198ac84d918b7a7604cf3e4e
eliseuSousa <eliseusousa759@gmail.com>
2024-06-20 13:16:18 -0300

    finalizando o prototipo

--- main.js
+++ main.js
@@ -1,16 +1,70 @@
 let board = [
-  ['O', 'X', 'O'],
-  ['X', 'X', 'O'],
-  ['X', 'O', 'X'],
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', ''],
 ];
 
-let player1 = 'X';
-let player2 = 'O';
+let players = ['X', 'O'];
+let currentPlayer;
+let available = [];
 let w, h;
 
 function setup() {
   let canvas = createCanvas(400, 400);
   canvas.parent('canvas-container');
+  frameRate(1);
+  currentPlayer = floor(random(players.length));
+  for (let j = 0; j < 3; j++) {
+    for (let i = 0; i < 3; i++) {
+      available.push([i,j])
+    }
+  }
+}
+
+function nexTurn() {
+  let index = floor(random(available.length));
+  let spot = available.splice(index, 1)[0];
+  let i = spot[0];
+  let j = spot[1];
+  board[i][j] = players[currentPlayer];
+  currentPlayer = (currentPlayer + 1) % players.length;
+}
+
+function equals3(a, b, c) {
+  return (a == b && b == c && a != '');
+}
+
+function checkWinner() {
+  let winner = null;
+
+  // Horizontal
+  for(let i = 0; i < 3; i++) {
+    if(equals3(board[i][0], board[i][1], board[i][2])) {
+      winner = board[i][0]
+    }
+  }
+
+  // Vertical
+  for(let i = 0; i < 3; i++) {
+    if(equals3(board[0][i], board[1][i], board[2][i])) {
+      winner = board[0][i]
+    }
+  }
+
+  // Diagonal
+  if(equals3(board[0][0], board[1][1], board[2][2])) {
+    winner = board[0][0];
+  }
+  if(equals3(board[2][0], board[1][1], board[0][2])) {
+    winner = board[2][0];
+  }
+
+  if(winner == null && available.length == 0) {
+    return 'tie';
+  } else {
+    return winner;
+  }
+
 }
 
 function draw() {
@@ -19,6 +73,11 @@ function draw() {
   w = width / 3;
   h = height / 3;
 
+  line(w, 0, w, height);
+  line(w*2, 0, w*2, height);
+  line(0, h, width, h);
+  line(0, h*2, width, h*2);
+
   for (let j = 0; j < 3; j++) {
     for (let i = 0; i < 3; i++) {
       let x = w * i + w / 2;
@@ -26,14 +85,24 @@ function draw() {
       let spot = board[j][i]; // Corrigir a ordem de indexação para [j][i]
       textSize(32);
       strokeWeight(4);
-      if (spot == player2) {
+      if (spot == players[1]) {
         noFill();
         ellipse(x, y, w / 2);
-      } else if (spot == player1) {
+      } else if (spot == players[0]) {
         let xr = w / 4;
         line(x - xr, y - xr, x + xr, y + xr);
         line(x + xr, y - xr, x - xr, y + xr);
       }
     }
   }
+
+  let result  = checkWinner();
+  if(result != null) {
+    noLoop();
+    console.log(result)
+    console.log(board);
+  } else {
+    nexTurn();
+  }
+  
 }
